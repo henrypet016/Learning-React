2a7e5efe41bc2992687b913e3b70b323e669242e
henrypet016 <h.pettitt@elifesciences.org>
2023-12-14 15:27:49 +0000

    completed so i only need to pull api once

--- api-pull-website/src/App.js
+++ api-pull-website/src/App.js
@@ -1,22 +1,32 @@
+import React, { useState, useEffect } from "react";
 import Axios from "axios";
-import {useState} from "react";
 
 function App() {
-    const [articleTitle, setArticleTitle] = useState("");
-    const [articleImpactStatement, setArticleImpactStatement] = useState("");
-    Axios.get("https://api.elifesciences.org/articles/85398").then((res) => {
-      setArticleTitle(res.data.title);
-    })
-    Axios.get("https://api.elifesciences.org/articles/85398").then((res) => {
-      setArticleImpactStatement(res.data.impactStatement);
-    })
+  const [articleData, setArticleData] = useState({
+    title: "",
+    impactStatement: ""
+  });
+
+  useEffect(() => {
+    Axios.get('https://api.elifesciences.org/articles/${articleNumber}')
+      .then((res) => {
+        setArticleData({
+          title: res.data.title,
+          impactStatement: res.data.impactStatement
+        });
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }, []);
+
   return (
     <div className="App">
       <button> Article 1 </button>
       <button> Article 2 </button>
       <button> Article 3 </button>
-      <h1> {articleTitle} </h1>
-      <p> {articleImpactStatement} </p>
+      <h1> {articleData.title} </h1>
+      <p> {articleData.impactStatement} </p>
     </div>
   );
 }
